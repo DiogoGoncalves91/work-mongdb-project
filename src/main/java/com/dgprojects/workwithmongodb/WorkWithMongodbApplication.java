a948449dc79af3b99b0d45a9fce37a7af197b243
Diogo Goncalves <diogogoncalves91@icloud.com>
2024-01-31 11:03:29 +0000

    Create project

---
+++ src/main/java/com/dgprojects/workwithmongodb/WorkWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.dgprojects.workwithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgprojects/workwithmongodb/WorkWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgprojects.workwithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
